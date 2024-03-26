f5d149fd0c6293601f8e6ecbbe7ffa6e8d0d9bf7
sayf63 <sayf6@live.com>
2024-03-26 18:25:29 +0000

    New utils.sh!

---
+++ utils.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#First question for utils.sh
+find -L /usr/bin -name "c*"
+#Second question for utils.sh
+find -L /usr/bin -type f -name "*.sh"
+#Third question for utils.sh
+find -L 
+
